d2412cbb6e0d70978f35c122d9ce6cee3b782f25
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-18 02:36:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ curl_close($curl); // Close request
   <title>Formulario contactos</title>
 </head>
 <body style="background: #818181;font-size: 2rem;">
-  <?php echo '<p  style="color: #00ff00;">HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
+  <?php echo '<p  style="color: #00ff00;">##### HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
  <?php echo '<p  style="color: #ffff00;>HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?>
 </body>
 </html>
