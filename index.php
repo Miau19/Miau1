409f4b5f86fc3e8deb702f0cccde0a7d68aa67b9
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-18 05:27:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,10 +38,7 @@ echo '</pre>';
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+  <meta charset="UTF-8"> 
   <title>Formulario contactos</title>
 </head>
 <body style="background: #818181;font-size: 2rem;">
