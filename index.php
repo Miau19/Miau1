ca2e26cdfcdcbba397d5bfafe2413ed49d7b7c6a
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-18 05:35:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -56,6 +56,6 @@ echo '</pre>';
   <?php echo '<p  style="color: #00ff00;">##### HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
   <?php echo '<p  style="color: #ffff00;">HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?>
 
-  <h1>h1 ¡Hola Mundo!</h1>
+  <h1>h1 ¡Hola Mundo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
 </body>
 </html>
