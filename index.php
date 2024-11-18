790771369404bf54df286de0529737207eb7f987
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-18 01:59:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php echo '<p>HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (contacto.php)</p>' ?>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+  <title>Formulario contactos</title>
+</head>
+<body style="background: #818181;color: #ff4d00;font-size: 2rem;">
+  <?php echo '<p>HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
+
+</body>
+</html>
