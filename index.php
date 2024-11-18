94c592fe2d0152882b519882048798f6c352f8ed
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-18 04:09:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+/**
+ * Requires curl enabled in php.ini
+ **/
+
+<?php
+$url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+$parameters = [
+  'start' => '1',
+  'limit' => '5000',
+  'convert' => 'USD'
+];
+
+$headers = [
+  'Accepts: application/json',
+  'X-CMC_PRO_API_KEY: cf14cf58-626e-49d9-aea8-a64c15e91dd2'
+];
+$qs = http_build_query($parameters); // query string encode the parameters
+$request = "{$url}?{$qs}"; // create the request URL
+
+
+$curl = curl_init(); // Get cURL resource
+// Set cURL options
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $request,            // set the request URL
+  CURLOPT_HTTPHEADER => $headers,     // set the headers 
+  CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+));
+
+$response = curl_exec($curl); // Send the request, save the response
+print_r(json_decode($response)); // print json decoded response
+curl_close($curl); // Close request
+?>
 
 
 <!DOCTYPE html>
@@ -13,6 +45,6 @@
   <?php echo '<p  style="color: #00ff00;">##### HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
   <?php echo '<p  style="color: #ffff00;">HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?>
 
-  <h1>h1 ¡Hola Mundo!></h1>
+  <h1>h1 ¡Hola Mundo!</h1>
 </body>
 </html>
