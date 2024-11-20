09000d52d701af7dcb4b2fc894f4d5d3ac4016d3
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-19 20:57:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
+<?php 
+
 $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
 $parameters = [
   'start' => '1',
-  'limit' => '50',
+  'limit' => '1',
   'convert' => 'USD'
 ];
 
@@ -12,7 +13,6 @@ $headers = [
 ];
 $qs = http_build_query($parameters); // query string encode the parameters
 $request = "{$url}?{$qs}"; // create the request URL
-
 
 $curl = curl_init(); // Get cURL resource
 // Set cURL options
@@ -25,22 +25,20 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl); // Send the request, save the response 
-////print_r(json_decode($response)); // print json decoded response
-$res=json_decode($response,true);
-//$uno=$res->data->quote->USD->price;
-///print_r(aMoneda($uno));
-//print_r($uno);
-/*
-function aMoneda($value){
- $value=($value='')?0:$value;
-  return number_format($value,2);
-}
-*/
+$datos=json_decode($response,true);
+
 //echo '<pre>';
 //  print_r($response); 
 //echo '</pre>';
+//print_r($datos); 
 
 curl_close($curl); // Close request
+
+// // Hora actual
+// echo date('h:i:s') . "<br>";
+// 
+// // Dormir durante 5 segundos
+// sleep(5); 
 
 ?>
 
@@ -50,24 +48,137 @@ curl_close($curl); // Close request
   <meta charset="UTF-8">
   <title>Formulario contactos</title>
 </head>
-<body style="background: #818181;font-size: 1rem;color: #cacaca;">
+<body id="onload" onload="" style="background: #333333;font-size: 1rem;color: #cacaca;">
 
-  <div class="row">
+  <div style="background: #222222;">
         
-    <?php foreach($res['data'] as $row) { ?>
-   <h4 style="color: #990000;">
-     <?php echo $row['name'] ?> (((<?php echo $row['symbol'] ?>)))  circulating_supply: <?php echo $row['circulating_supply'] ?>   total_supply: <?php echo $row['total_supply'] ?>  
-   </h4>
-    <?php } ?>
-  
+    <?php foreach($datos['data'] as $row) { ?>
+
+   <div id="texto0" style="color: #550099;">
+     <?php print_r($row['quote']) ?> <?php echo '<br><br>' ?> 
+   </div><br>
+
+  <div style="color: #ff00ff;">
+     (((<?php echo $row['name'] ?>))) (((<?php echo $row['symbol'] ?>)))  
+     circulating_supply: <?php echo $row['circulating_supply'] ?>   total_supply: <?php echo $row['total_supply'] ?>  
+  </div><br>
+
+  <?php } ?>
+
+  Coinmarketcap(cambio cada 60s, actualizar pagina):
+  <div id="texto" style="color: #00ff00;background: rgb(12, 0, 24);   ;display: flex;justify-content: start;align-items: center;text-align: left;">
+  </div><br>
 
   </div>
 
   <?php echo '<p  style="color: #0000ff;">##### HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?> 
   <?php echo '<p  style="color: #ffff00;">HOLA MUNDO !!!!!!!!!!</p><p>HOLA MUNDO !!!!!!!!!! (index.php)</p>' ?>
 
-  <h1>¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡Hola Mundo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!h1>
+  ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡Hola Mundo!!!!!!!!!!!!!!>
 
-    <script src="index.js"></script>
+  <!-- <script src="index.js"></script> -->
+
+  <script>
+    function F1(m,n) { //FORMATEA CON n DECIMALES 
+     let cadena= new String(m), rgx = /(\d+)(\d{3})/, ceros='',nuevaCadena='', decimal, d, nuevaParteEntera='', e; //console.log('cadena====',cadena);//var x = cadena.replace(/,/g,"").split(".");
+     (n==undefined || n==0 || n=='')? n=Number(n) : n=Number(n)//n=2: PARA UN RETORNO CON DOS DECIMALES COMO MINIMO
+     if (/[a-zA-Z]/.test(cadena)==true) { //SI ENCUENTRA LETRAS EN LA CADENA: infinity NaN isNaN 5.554680159996e+333
+       return cadena; 
+     }
+     else {//SI ENCUENTRA SOLO NUMEROS Y UN PUNTO EN LA CADENA
+         if (/[.]/.test(cadena)==true) {//SI ENCUENTRA PUNTO EN LA CADENA
+             e = cadena.split('.')[0];//d: PARTE ENTERA
+             d = cadena.split('.')[1];//d: PARTE DECIMAL
+             if (d.length >=n) {
+                 decimal = d.substring(0, n);//CORTAR CADENA DE DECIMALES A n DIGITOS  
+             }
+             if (d.length < n) {
+                 for (let i = 1; i <= n-d.length; i++) {ceros += '0';}//COMPLETA PARTE DECIMAL CON CEROS
+                 decimal = d+ceros;//console.log('decimal....',decimal);
+             }
+             //AGREGAR COMAS A LA PARTE ENTERA:
+             for (i = 1; i <= Math.trunc(e.length/3); i++) {// trunc OBTIENE LA PARTE ENTERA DE: LA LONGITUD DE e DIVIDIDO POR 3
+                 e = e.replace(rgx, '$1' + ',' + '$2');  
+             } 
+             (n==undefined || n==0 || n=='')? nuevaParteEntera=e+'' : nuevaParteEntera=e+'.'+decimal;
+             return nuevaParteEntera;
+         } 
+         else {
+             for (let i = 1; i <= n; i++) {ceros += '0';}//GENERAR CEROS PARA PARTE DECIMAL DE 2CADENA
+             //AGREGAR COMAS A LA CADENA:
+             for (let i = 1; i <= Math.trunc(cadena.length/3); i++) {// trunc OBTIENE LA PARTE ENTERA DE: LA LONGITUD DE e DIVIDIDO POR 3
+                 cadena = cadena.replace(rgx, '$1' + ',' + '$2');  
+             } 
+             (n==undefined || n==0 || n=='')? nuevaCadena = cadena : nuevaCadena = cadena+'.'+ceros;
+             return nuevaCadena;
+         }
+     }
+    } 
+
+    function datos() {
+     /*
+     @[price] => 92100.810078196
+     @[volume_24h] => 72981638354.878
+     @[volume_change_24h] => -4.0925
+     @[percent_change_1h] => -0.97120962
+     @[percent_change_24h] => 0.37430519
+     @[percent_change_7d] => 3.55252826
+     @[percent_change_30d] => 34.04256665
+     @[percent_change_60d] => 45.56037248
+     @[percent_change_90d] => 50.48512072
+     @[market_cap] => 1822180265895.8
+     @[market_cap_dominance] => 59.3449
+     @[fully_diluted_market_cap] => 1934117011642.1
+     */
+     let t0=texto0.textContent.replace(/[[]/g,'@[');
+       let t=t0.split('@').slice(2,14);
+      let price                = t[0].replace('[price] => ','')
+      ,volume_24h              = t[1].replace('[volume_24h] => ','')
+      ,volume_change_24h       = t[2].replace('[volume_change_24h] => ','')
+      ,percent_change_1h       = t[3].replace('[percent_change_1h] => ','')
+      ,percent_change_24h      = t[4].replace('[percent_change_24h] => ','')
+      ,percent_change_7d       = t[5].replace('[percent_change_7d] => ','')
+      ,percent_change_30d      = t[6].replace('[percent_change_30d] => ','')
+      ,percent_change_60d      = t[7].replace('[percent_change_60d] => ','')
+      ,percent_change_90d      = t[8].replace('[percent_change_90d] => ','')
+      ,market_cap              = t[9].replace('[market_cap] => ','')
+      ,market_cap_dominance    =t[10].replace('[market_cap_dominance] => ','')
+      ,fully_diluted_market_cap=t[11].replace('[fully_diluted_market_cap] => ','')
+      ; 
+      document.getElementById('texto').innerHTML=
+       'Sin formato:'+'<br>'
+      +'price                    : '+price                   +'<br>'
+      +'volume_24h               : '+volume_24h              +'<br>'
+      +'volume_change_24h        : '+volume_change_24h       +'<br>'
+      +'percent_change_1h        : '+percent_change_1h       +'<br>'
+      +'percent_change_24h       : '+percent_change_24h      +'<br>'
+      +'percent_change_7d        : '+percent_change_7d       +'<br>'
+      +'percent_change_30d       : '+percent_change_30d      +'<br>'
+      +'percent_change_60d       : '+percent_change_60d      +'<br>'
+      +'percent_change_90d       : '+percent_change_90d      +'<br>'
+      +'market_cap               : '+market_cap              +'<br>'
+      +'market_cap_dominance     : '+market_cap_dominance    +'<br>'
+      +'fully_diluted_market_cap : '+fully_diluted_market_cap+'<br>'+'<br>'
+
+      +'Con formato:'+'<br>'
+      +'price                    : '+F1(price                   ,9)+'<br>'
+      +'volume_24h               : '+F1(volume_24h              ,9)+'<br>'
+      +'volume_change_24h        : '+F1(volume_change_24h       ,9)+'<br>'
+      +'percent_change_1h        : '+F1(percent_change_1h       ,9)+'<br>'
+      +'percent_change_24h       : '+F1(percent_change_24h      ,9)+'<br>'
+      +'percent_change_7d        : '+F1(percent_change_7d       ,9)+'<br>'
+      +'percent_change_30d       : '+F1(percent_change_30d      ,9)+'<br>'
+      +'percent_change_60d       : '+F1(percent_change_60d      ,9)+'<br>'
+      +'percent_change_90d       : '+F1(percent_change_90d      ,9)+'<br>'
+      +'market_cap               : '+F1(market_cap              ,9)+'<br>'
+      +'market_cap_dominance     : '+F1(market_cap_dominance    ,9)+'<br>'
+      +'fully_diluted_market_cap : '+F1(fully_diluted_market_cap,9)+'<br>'
+      ;
+    }
+    datos();
+    // var timerID; 
+    // function startTimer() {timerID=window.setInterval(datos,1000);}
+    // startTimer() ;
+  </script>
 </body>
 </html>
