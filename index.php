8fc157b3927850e00256b6ea8ec8ba2d0fa2b733
Miau19 <107598923+Miau19@users.noreply.github.com>
2024-11-24 01:00:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background: #6d6d6d;">
-    <?php echo '<input type="text" value="0"> <input type="text" style="border: solid 1px #7e7e7e;">' ?>
+<body style="background: #222222;">
+    <?php echo
+     '<input type="text" value="0" style="background: #00aaaa;border: solid 1px #7e7e7e;padding: 0.5rem;" >
+      <input type="text" style="background: #00aaaa;border: solid 1px #7e7e7e;padding: 0.5rem;" >'
+    ?>
 
     <!-- <iframe src="https://miau1-hqbpfjgpctgdb3ej.canadacentral-01.azurewebsites.net/" frameborder="0" width="100%" height="700"></iframe> -->
     <iframe src="iframeindex.php" frameborder="0" width="100%" height="700" style="background: rgba(0, 0, 20, 1);"></iframe> 
